b94fa96b05ba11857516e0882c9654c6c9c82329
Jason Fernandes <18moneyj@gmail.com>
2021-10-20 14:17:12 -0500

    customerSelect Complete

---
+++ forms/customerSelect/customerSelect.js
@@ -0,0 +1,25 @@
+let query = ' '
+let req = {}
+netID = 'jef58611'
+let pw = 'MuUqe86@>^A"8!h4'
+let results = []
+
+customerSelect.onshow = function() {
+    query = "SELECT * FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        console.log(`the results are \n ${results}`)
+        if (results.length == 0)
+            lblMessage.value = "There are no customers in the database."
+        else {
+            let message = ""
+            for (i = 0; i < results.length; i++)
+                message = message + results[i][1] + "\n"
+            txtaCustomerNames.value = message
+        } // end else
+
+    } else // the transit didn't work - bad wifi? server turned off?
+        lblMessage.value = "Error code: " + req.status
+}
